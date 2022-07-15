40f71e2626172d2dea342fbbba4d0da2506c4ea3
muhamad.zolfaghari <you@example.com>
2022-07-15 15:35:14 +0430

    The api/shorturl was updated because of the project url has no dns so this must be excluded from validate by dns.lookup.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const app = express();
 const isNumber = require("is-number");
 const dns = require("dns");
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
@@ -25,16 +23,17 @@ app.get("/api/hello", function (req, res) {
 
 const shortUrls = [""];
 
-const PROJECT_URL = "http://localhost:3000";
-
 app.post("/api/shorturl/", (req, res) => {
-  const url = req.body.url;
-  dns.lookup(url, (err) => {
-    if (!url.includes(PROJECT_URL) && err) {
-      return res.json({ error: "invalid url" });
-    }
-    const short_url = shortUrls.push(url) - 1;
-    const body = { original_url: url, short_url: short_url };
+  let hostname;
+  try {
+    hostname = new URL(req.body.url).hostname;
+  } catch (error) {
+    hostname = req.body.url;
+  }
+  dns.lookup(hostname, (err) => {
+    if (err) return res.json({ error: "invalid url" });
+    const short_url = shortUrls.push(req.body.url) - 1;
+    const body = { original_url: req.body.url, short_url: short_url };
     res.json(body);
   });
 });
@@ -49,6 +48,6 @@ app.get("/api/shorturl/:short_url", (req, res) => {
   res.redirect(shortUrls[shortUrl]);
 });
 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(process.env.PORT)
 });
